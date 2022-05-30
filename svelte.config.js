4854afaa55439c5156836eac8ee87ece3cadb347
Sylvain Laurent <s@6120.eu>
2022-05-30 18:13:26 +0200

    Add NodeGlobalPolyfillPlugin to vite plugins

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
-import polyfillNode from 'rollup-plugin-polyfill-node';
+import preprocess from 'svelte-preprocess';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	plugins: [polyfillNode()],
-
-	optimizeDeps: {
-		exclude: ['dragula']
-	},
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter(),
 
@@ -23,7 +21,6 @@ const config = {
 			lib: 'svelte/src/lib',
 			routes: 'svelte/src/routes',
 			params: 'svelte/src/params',
-			routes: 'svelte/src/routes',
 			serviceWorker: 'svelte/src/service-worker',
 			template: 'svelte/src/app.html'
 		},
@@ -31,13 +28,36 @@ const config = {
 			assets: '',
 			base: ''
 	    },
-    vite: {
-        server: {
-            fs: {
-                allow: ['..']
-            }
-        }
-    }
+		vite: {
+			server: {
+				fs: {
+					allow: ['..']
+				}
+			},
+			optimizeDeps: {
+				esbuildOptions: {
+					// Node.js global to browser globalThis
+					define: {
+						global: 'globalThis'
+					},
+					// Enable esbuild polyfill plugins
+					plugins: [
+						NodeGlobalsPolyfillPlugin({
+							buffer: true,
+							global: true,
+							process: true,
+							url: true,
+							assert: true,
+							crypto: true,
+							http: true,
+							https: true,
+							os: true,
+							stream: true
+						})
+					]
+				}
+			},
+		}
 	},
 };
 
--- svelte/src/routes/starknet.svelte
+++ svelte/src/routes/starknet.svelte
@@ -6,6 +6,31 @@
 
 <script lang="ts">
 	import Counter from '$lib/Counter.svelte';
+
+	import { onMount } from 'svelte';
+	import {Contract, json, Provider} from 'starknet';
+	import { writable } from 'svelte/store';
+
+	const contractAddr = '0x04516dd716ce43ea93ed1479baf811b0156e432aef855b458a465fe1299761bb';
+	const abi = json.parse("[{\"inputs\":[],\"name\":\"count\",\"outputs\":[{\"name\":\"count\",\"type\":\"felt\"}],\"type\":\"function\"},{\"inputs\":[],\"name\":\"getCounter\",\"outputs\":[{\"name\":\"count\",\"type\":\"felt\"},{\"name\":\"on_stark_net\",\"type\":\"felt\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"get_l1_gateway\",\"outputs\":[{\"name\":\"_l1_gateway\",\"type\":\"felt\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"name\":\"from_address\",\"type\":\"felt\"},{\"name\":\"_counter\",\"type\":\"felt\"}],\"name\":\"EVMtoSN\",\"outputs\":[],\"type\":\"l1_handler\"},{\"inputs\":[],\"name\":\"SNtoEVM\",\"outputs\":[],\"type\":\"function\"}]");
+	const counte = writable(0);
+	let countValue;
+
+	counte.subscribe(value => {
+		countValue = value;
+		console.log(countValue)
+	})
+	onMount(async () => {
+		const provider = new Provider({
+			baseUrl: 'https://alpha4.starknet.io',
+			feederGatewayUrl: 'feeder_gateway',
+			gatewayUrl: 'gateway',
+		});
+
+		const counter = new Contract(abi, contractAddr, provider);
+		const {count, on_stark_net} = await counter.getCounter();
+		counte.set(count.toNumber());
+	});
 </script>
 
 <svelte:head>
@@ -14,7 +39,7 @@
 </svelte:head>
 
 <section>
-	<Counter />
+	<Counter /> {countValue}
 </section>
 
 <style>
